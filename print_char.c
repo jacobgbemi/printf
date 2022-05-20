ef51cf1077c57512db02f9af78a4061b7fb662ee
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-05-20 14:35:26 +0000

    add print char function

---
+++ print_char.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_car - print char as string
+  * @ap: char to print
+  * Return: string
+  */
+
+char *print_char(va_list ap)
+{
+	char *ptr;
+	char c;
+
+	c = va_arg(ap, int);
+	if (c == 0)
+		c = '\0';
+	ptr = malloc(sizeof(char) * 2);
+	if (ptr == NULL)
+		return (NULL);
+	ptr[0] = c;
+	ptr[1] = '\0';
+
+	return (ptr);
+}
