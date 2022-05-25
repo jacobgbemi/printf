72feb9a391426a8f8932db1064cd4f1d0a4e86bd
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-05-25 12:36:03 +0000

    made change to _printf

--- _printf.c
+++ _printf.c
@@ -12,60 +12,90 @@ int _printf(const char *format, ...)
 	char* (*f)(va_list ap);
 
 	buffer = string_buffer();
+	if (buffer == NULL)
+		return (-1);
+
+	if (format == NULL)
+		return (-1);
+
 	va_start(ap, format);
-	for (i = 0; format[i] != '\0'; i++)
+
+
+	while (format[i] != '\0')
 	{
-		len = buffer_overflow(buffer, len);
 		if (format[i] != '%')
 		{
-			buffer[len++] = format[i++];
+			len = buffer_overflow(buffer, len);
+			buffer[len] = format[i];
+			len++;
+			i++;
+			total++;
 		}
 		else
 		{
 			i++;
+			if (format[i] == '\0')
+			{
+				va_end(ap);
+				free(buffer);
+				return (-1);
+			}
+
 			if (format[i] == '%')
 			{
-				buffer[len++] = format[i];
+				len = buffer_overflow(buffer, len);
+				buffer[len] = format[i];
+				len++;
+				total++;
 			}
 			else
 			{
 				f = select_func(format[i]);
-				if (f != NULL)
+				if (f == NULL)
+				{
+					len = buffer_overflow(buffer, len);
+					buffer[len] = '%';
+					total++;
+					buffer[len] = format[i];
+					total++;
+				}
+				else
 				{
 					str = f(ap);
-					for (j = 0; str[j] != '\0'; j++)
+					if (str == NULL)
 					{
-						buffer[len++] = str[j++];
+						va_end(ap);
+						free(buffer);
+						return (-1);
 					}
+
+					if (format[i] == 'c' && str[0] == '\0')
+					{
+						len = buffer_overflow(buffer, len);
+						buffer[len] = '\0';
+						len++;
+						total++;
+					}
+
+					while (str[j] != '\0')
+					{
+						len = buffer_overflow(buffer, len);
+						buffer[len] = str[j];
+						len++;
+						total++;
+						j++;
+					}
+					free(str);
 				}
 			}
-			total++;
+			i++;
 		}
+
 	}
-	free(buffer);
+
 	realloc_buffer(buffer, len, ap);
 	return (total);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
